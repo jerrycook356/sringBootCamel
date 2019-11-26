4a7f8f99fe7ec73021587b3914773e19e2f3526f
Cook <COOKJ4@legal.regn.net>
2019-11-26 18:10:05 -0500

    added application.yml

--- src/main/java/com/learncamel/Route/SimpleCamelRoute.java
+++ src/main/java/com/learncamel/Route/SimpleCamelRoute.java
@@ -1,11 +1,15 @@
 package com.learncamel.Route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCamelRoute extends RouteBuilder {
 
+    @Autowired
+    Environment environment;
 
     @Override
     public void configure() throws Exception {
@@ -13,9 +17,9 @@ public class SimpleCamelRoute extends RouteBuilder {
         //will copy the contents of the directory to the data/output directory if new files are present
         //will not poll directory if there is no new files,will poll again when there is new files
 
-        from("timer:hello?period=10")
-                .log("Timer invoked and the body is ${body}")
-                .pollEnrich("file:data/input?delete=true&readLock=none")
-                .to("file:data/output");
+        from("{{startRoute}}")
+                .log("Timer invoked and the body "+ environment.getProperty("message"))
+                .pollEnrich("{{fromRoute}}")
+                .to("{{toRoute1}}");
     }
 }
